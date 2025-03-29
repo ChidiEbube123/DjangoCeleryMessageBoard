f8293b96630d219065f51bdbdb655a3f9b775b63
ChidiEbube123 <fleektyre@gmail.com>
2025-03-29 12:15:40 +0100

    about to use s3

--- a_core/settings.py
+++ a_core/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'a_core.wsgi.application'
-
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",#corr
@@ -92,16 +92,16 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-
+'''
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 
 
